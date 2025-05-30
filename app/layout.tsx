8644a15a503009efb974b25970b6e66ac163fbac
Dan Betz <dbetz@donorschoose.org>
2025-05-29 20:10:50 -0400

    adding favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
-// app/layout.tsx
 import "../styles/globals.css";
 import { ReactNode } from "react";
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Noto_Serif } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Noto_Serif } from "next/font/google";
 
 const noto = Noto_Serif({
-  subsets: ['latin'],
-  weight: ['400','300','200'],
-  style: ['normal','italic'],
-  display: 'swap',
+  subsets: ["latin"],
+  weight: ["400", "300", "200"],
+  style: ["normal", "italic"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -20,6 +19,17 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicon & meta tags */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`${noto.className} bg-white text-black font-sans`}>
         {children}
         <Analytics />
