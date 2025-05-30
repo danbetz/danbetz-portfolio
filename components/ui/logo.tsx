1fc6b393a53657355974e868476289706035f7be
Dan Betz <dbetz@donorschoose.org>
2025-05-30 18:01:51 -0400

    mobile fixes

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -6,26 +6,26 @@ export default function Logo({ active = false }: { active?: boolean }) {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 659.87 478.18"
-        width="80"
-        height="40"
+        width="40"
+        height="auto"
         role="img"
         aria-label="db logo"
       >
         <g>
           {/* Dark gray group (color #474649) */}
-          <circle className="dot dot-1" fill="#474649" cx="420.52" cy="63.51" r="63.51" />
-          <circle className="dot dot-2" fill="#474649" cx="420.52" cy="239.09" r="63.51" />
-          <circle className="dot dot-3" fill="#474649" cx="420.52" cy="414.67" r="63.51" />
-          <circle className="dot dot-4" fill="#474649" cx="596.36" cy="414.67" r="63.51" />
-          <circle className="dot dot-5" fill="#474649" cx="596.36" cy="237.38" r="63.51" />
+          <circle className="dot dot-1" fill="oklch(0.5478 0.007 106.523)" cx="420.52" cy="63.51" r="63.51" />
+          <circle className="dot dot-2" fill="oklch(0.5478 0.007 106.523)" cx="420.52" cy="239.09" r="63.51" />
+          <circle className="dot dot-3" fill="oklch(0.5478 0.007 106.523)" cx="420.52" cy="414.67" r="63.51" />
+          <circle className="dot dot-4" fill="oklch(0.5478 0.007 106.523)" cx="596.36" cy="414.67" r="63.51" />
+          <circle className="dot dot-5" fill="oklch(0.5478 0.007 106.523)" cx="596.36" cy="237.38" r="63.51" />
         </g>
         <g>
           {/* Black group (color #242426) */}
-          <circle className="dot dot-6" fill="#242426" cx="239.35" cy="63.51" r="63.51" />
-          <circle className="dot dot-7" fill="#242426" cx="243.5" cy="239.09" r="63.51" />
-          <circle className="dot dot-8" fill="#242426" cx="239.35" cy="414.67" r="63.51" />
-          <circle className="dot dot-9" fill="#242426" cx="63.51" cy="414.67" r="63.51" />
-          <circle className="dot dot-10" fill="#242426" cx="63.51" cy="237.38" r="63.51" />
+          <circle className="dot dot-6" fill="oklch(0.2921 0.007 106.523)" cx="239.35" cy="63.51" r="63.51" />
+          <circle className="dot dot-7" fill="oklch(0.2921 0.007 106.523)" cx="243.5" cy="239.09" r="63.51" />
+          <circle className="dot dot-8" fill="oklch(0.2921 0.007 106.523)" cx="239.35" cy="414.67" r="63.51" />
+          <circle className="dot dot-9" fill="oklch(0.2921 0.007 106.523)" cx="63.51" cy="414.67" r="63.51" />
+          <circle className="dot dot-10" fill="oklch(0.2921 0.007 106.523)" cx="63.51" cy="237.38" r="63.51" />
         </g>
       </svg>
     </span>
